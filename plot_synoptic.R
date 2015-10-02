d1ae2492e979aca76f3f4a295027cfe831fd0800
Jeff Walker <walker.jeff.d@gmail.com>
2015-10-02 11:59:35 -0400

    add synoptic plot and csv output

--- plot_synoptic.R
+++ plot_synoptic.R
@@ -8,28 +8,19 @@ theme_set(theme_bw())
 
 rm(list=ls())
 
+source('functions.R')
+
 load("kt_synoptic.Rdata")
 load("gis.Rdata")
 
-wq.kt_synoptic <- mutate(wq.kt_synoptic,
-                         VAR = ordered(as.character(VAR), levels=c("TP", "PO4", "TN", "NH4", "NO23")))
+variables <- c("TP", "PO4", "TN", "NH4", "NO23")
+
+wq.kt_synoptic <- filter(wq.kt_synoptic,
+                         VAR %in% variables) %>%
+  mutate(VAR = ordered(as.character(VAR),
+                       levels=variables))
 
 synoptic_stn <- c("SR0100", "SR0120", "SR0200")
-
-map <- get_stamenmap(bbox=c(-122.1, 42.15, -120.6, 43), zoom=10)
-
-makeFootnote <- function(footnoteText='Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under CC BY SA.',
-                         size=0.7, color=grey(0.5)) {
-  #   http://ryouready.wordpress.com/2009/02/17/r-good-practice-adding-footnotes-to-graphics/
-  require(grid)
-  pushViewport(viewport())
-  grid.text(label= footnoteText ,
-            x = unit(1,"npc") - unit(2, "mm"),
-            y= unit(2, "mm"),
-            just=c("right", "bottom"),
-            gp=gpar(cex= size, col=color))
-  popViewport()
-}
 
 pdf(file.path("pdf", "synoptic-data.pdf"), width=11, height=8.5)
 
@@ -38,7 +29,7 @@ p <- ggmap(map, extent = 'device', darken = c(0.2, 'white')) +
                color = 'black', alpha = 0, size = 0.5) +
   geom_path(aes(x = long, y = lat, group = group), data = flowline,
             color='deepskyblue', size=0.2) +
-  geom_polygon(aes(x = long, y = lat, group = group), data = incbasin_ivory,
+  geom_polygon(aes(x = long, y = lat, group = group), data = incbasin,
                color = 'orangered', fill = 'grey50', alpha = 0.2, size = 0.2) +
   geom_point(aes(x = LONGITUDE, y = LATITUDE), data = filter(stn.kt_synoptic, SITE %in% synoptic_stn),
              shape=21, fill='deepskyblue', size=3) +
@@ -71,3 +62,63 @@ p <- filter(wq.kt_synoptic, SITE %in% synoptic_stn) %>%
 print(p)
 
 dev.off()
+
+filter(wq.kt_synoptic, SITE %in% synoptic_stn,
+       VAR == "TP") %>%
+  group_by(SITE, VAR) %>%
+  summarise(N=n())
+
+p <- filter(wq.kt_synoptic, SITE %in% synoptic_stn) %>%
+  filter(VAR %in% c("TP")) %>%
+  ggplot(aes(SITE_DESCRIPTION, VALUE)) +
+  geom_boxplot() +
+  geom_point(color='orangered') +
+  theme(axis.text.x=element_text(angle=45, hjust=1, vjust=1)) +
+  ylim(0, NA) +
+  labs(x="", y="Concentration (ppm)", title="Distributions of Synoptic WQ Data")
+print(p)
+
+p <- filter(wq.kt_synoptic, SITE %in% synoptic_stn) %>%
+  filter(VAR %in% c("TP")) %>%
+  ggplot(aes(SITE_DESCRIPTION, VALUE)) +
+  geom_boxplot() +
+  geom_point(color='orangered') +
+  theme(axis.text.x=element_text(angle=45, hjust=1, vjust=1)) +
+  ylim(0, NA) +
+  labs(x="", y="Concentration (ppm)", title="Distributions of Synoptic WQ Data")
+print(p)
+
+load('kt_sprague.Rdata')
+
+
+
+tp_medians <- filter(wq.kt_synoptic,
+                     SITE %in% synoptic_stn,
+                     VAR == "TP") %>%
+  group_by(SITE, SITE_DESCRIPTION) %>%
+  summarise(MEDIAN=median(VALUE*1000),
+            N=n())
+
+filename <- 'report/synoptic-tp.png'
+cat('Saving report figure to:', filename, '\n')
+png(filename, width=8, height=3, res=200, units='in')
+
+p <- filter(wq.kt_synoptic, SITE %in% synoptic_stn) %>%
+  filter(VAR %in% c("TP")) %>%
+  ggplot(aes(DATETIME, VALUE*1000)) +
+  geom_point() +
+  geom_hline(aes(yintercept=MEDIAN, color="Median"), data=tp_medians, linetype=2,
+             show_guide=TRUE) +
+  scale_color_manual('', values='red') +
+  facet_grid(~SITE_DESCRIPTION, scales="free_y") +
+  scale_x_datetime(labels=scales::date_format("%m/%Y")) +
+  scale_y_continuous(limits=c(0, NA)) +
+  theme(axis.text.x=element_text(angle=90, hjust=1, vjust=0.5),
+        strip.background=element_blank(),
+        strip.text=element_text(face='bold')) +
+  labs(x="Date", y="TP Concentration (ppb)")
+print(p)
+
+dev.off()
+
+write.csv(tp_medians, file='csv/synoptic_median_tp.csv', row.names=FALSE)
